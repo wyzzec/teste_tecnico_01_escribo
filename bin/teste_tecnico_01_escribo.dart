71badf06c99337a8747babfac0e4568c8ce3b5ca
wyzzec <andreluizalvesp@gmail.com>
2022-08-09 17:12:39 -0300

    teste tecnico completo com apenas uma função

--- bin/teste_tecnico_01_escribo.dart
+++ bin/teste_tecnico_01_escribo.dart
@@ -6,20 +6,8 @@ void main() {
     if (numero == 0){
       return 0;
     }
-    for (int i = 3; i < numero; i++){
-      if (i % 3 == 0){
-        somaTotal = somaTotal + i;
-      }
-    }
-    return somaTotal;
-  }
-  int somaValoresDivisiveisPor5 (int numero) {
-    int somaTotal = 0;
-    if (numero == 0){
-      return 0;
-    }
-    for (int i = 5; i < numero; i++){
-      if (i % 5 == 0){
+    for (int i = 0; i < numero; i++){
+      if (i % 3 == 0 || i % 5 == 0){
         somaTotal = somaTotal + i;
       }
     }
@@ -31,7 +19,7 @@ void main() {
     print('Digite o numero divisivel por 3 ou 5:\n');
     String? numeroDigitadoPeloUsuario = stdin.readLineSync();
     if (numeroDigitadoPeloUsuario != null){
-      print('Soma total de divisiveis por 3 ou 5: ${somaValoresDivisiveisPor3(int.parse(numeroDigitadoPeloUsuario)) + somaValoresDivisiveisPor5(int.parse(numeroDigitadoPeloUsuario))}\n');
+      print('Soma total de divisiveis por 3 ou 5: ${somaValoresDivisiveisPor3(int.parse(numeroDigitadoPeloUsuario))}\n');
       print('Deseja fazer outro calculo? (y/n)\n');
       String? escolhaContinuar = stdin.readLineSync();
       if (escolhaContinuar == 'n'){
