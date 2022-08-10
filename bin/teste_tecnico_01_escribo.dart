20b330cf7b2d4c8199db08bd21a940d6f42ef672
wyzzec <andreluizalvesp@gmail.com>
2022-08-10 10:21:42 -0300

    tratamento de erros

--- bin/teste_tecnico_01_escribo.dart
+++ bin/teste_tecnico_01_escribo.dart
@@ -3,11 +3,11 @@ import 'dart:io';
 void main() {
   int somaValoresDivisiveisPor3ou5 (int numero) {
     int somaTotal = 0;
-    if (numero == 0){
+    if (numero <= 0) {
       return 0;
     }
-    for (int i = 0; i < numero; i++){
-      if (i % 3 == 0 || i % 5 == 0){
+    for (int i = 0; i < numero; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
         somaTotal = somaTotal + i;
       }
     }
@@ -15,14 +15,27 @@ void main() {
   }
 
   bool loop = true;
+  int soma = 0;
   do {
+    bool numeroEhValido = true;
     print('Digite o numero divisivel por 3 ou 5:\n');
     String? numeroDigitadoPeloUsuario = stdin.readLineSync();
-    if (numeroDigitadoPeloUsuario != null){
-      print('Soma total de divisiveis por 3 ou 5: ${somaValoresDivisiveisPor3ou5(int.parse(numeroDigitadoPeloUsuario))}\n');
+    if (numeroDigitadoPeloUsuario != null) {
+      try {
+        soma = somaValoresDivisiveisPor3ou5(int.parse(numeroDigitadoPeloUsuario));
+        if (soma == 0) {
+          throw Exception();
+        }
+      } catch (e) {
+        print('Número deve ser inteiro e maior que 0');
+        numeroEhValido = false;
+      }
+      if (numeroEhValido) {
+        print('Soma total de divisiveis por 3 ou 5: $soma\n');
+      }
       print('Deseja fazer outro calculo? (y/n)\n');
       String? escolhaContinuar = stdin.readLineSync();
-      if (escolhaContinuar == 'n'){
+      if (escolhaContinuar == 'n') {
         loop = false;
       }
     }
